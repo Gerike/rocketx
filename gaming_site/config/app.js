b61eb610854b8b2a46e4e6dede4e26b2604c3ab0
Gerike <Gerike1995@gmail.com>
2016-11-14 20:16:40 +0100

    Small refactor to resource_loader.js
    Missing things, code reformat.

--- gaming_site/config/app.js
+++ gaming_site/config/app.js
@@ -126,7 +126,7 @@ module.exports = {
     | Dot Files
     |--------------------------------------------------------------------------
     |
-    | Define how to treat dot files when trying to server static resources.
+    | Define how to treat dot files when trying to server static resources_location.
     | By default it is set to ignore, which will pretend that dotfiles
     | does not exists.
     |
--- gaming_site/public/resource_loader.js
+++ gaming_site/public/resource_loader.js
@@ -3,47 +3,52 @@
  */
 'use strict'
 
-var entities = [];
-var resources = ['/assets/ship.png', '/assets/base_laser.png'];
-var canvas = document.getElementById('game_canvas');
-var ctx = canvas.getContext('2d');
-var resource_images = {};
+const canvas = document.getElementById('game_canvas');
+const ctx = canvas.getContext('2d');
+const resources_location = ['/assets/ship.png', '/assets/base_laser.png'];
 
-function setUpLoadingScreen(ctx){
-  ctx.font="40px Georgia";
+var entities = [];
+var resources = {};
+
+function setUpLoadingScreen(ctx) {
+  ctx.font = "40px Georgia";
   ctx.fillStyle = 'white';
-  ctx.fillText("Loading in progress",canvas.width/2 - 180, canvas.height/2 - 50);
-  ctx.rect(canvas.width/2 - 100, canvas.height/2 - 20, 200, 40);
-  ctx.strokeStyle="white";
+  ctx.fillText("Loading in progress", canvas.width / 2 - 180, canvas.height / 2 - 50);
+  ctx.rect(canvas.width / 2 - 100, canvas.height / 2 - 20, 200, 40);
+  ctx.strokeStyle = "white";
   ctx.stroke();
 }
 
-function updateLoaderBar(ctx, progress, all){
+function updateLoaderBar(ctx, progress, all) {
   ctx.fillRect(canvas.width / 2 - 100, canvas.height / 2 - 20, 200 / all * progress, 40);
   ctx.strokeStyle = "white";
   ctx.stroke();
 }
 
-function loadResources(ctx, counter){
-  if (counter < resources.length) {
+function getFileName(path) {
+  return path.split('/').pop().split('.')[0]
+}
+
+function loadResources(ctx, counter) {
+  if (counter < resources_location.length) {
     let temp_img = document.createElement('img');
     temp_img.onload = function () {
-      resource_images[resources[counter].split('/').pop().split('.')[0]] = temp_img;
-      updateLoaderBar(ctx, counter + 1, resources.length)
+      resources[getFileName(resources_location[counter])] = temp_img;
+      updateLoaderBar(ctx, counter + 1, resources_location.length)
       loadResources(ctx, counter += 1);
     };
-    temp_img.src = resources[counter];
+    temp_img.src = resources_location[counter];
   }
   else
     prepareGame();
 }
 
-function prepareGame(){
-  entities.push(new PlayerShip(0, 0, resource_images['ship'], [new BaseCannon(new BaseAmmo(1,resource_images['base_laser'],2), 1)], []));
+function prepareGame() {
+  entities.push(new PlayerShip(0, 0, resources['ship'], [new BaseCannon(new BaseAmmo(1, resources['base_laser'], 2), 1)], []));
   //Game ready to play => startGame();
 }
 
-function startLoad(ctx){
+function startLoad(ctx) {
   setUpLoadingScreen(ctx);
   loadResources(ctx, 0);
 }
