db664fc464322865f8f12a92fa36a49d04d3b8e7
Gerike <Gerike1995@gmail.com>
2016-11-15 22:59:52 +0100

    Added new resources (ships, laser)

--- RocketX/public/game_classes/Factories/ShipFactory.js
+++ RocketX/public/game_classes/Factories/ShipFactory.js
@@ -4,10 +4,10 @@
 'use strict'
 class ShipFactory {
   static createBasePlayerShip(x, y) {
-    return new PlayerShip(x, y, resources['ship'], [WeaponFactory.createBaseCannon(resources['ship'])], [], 4);
+    return new PlayerShip(x, y, resources['ship_2'], [WeaponFactory.createBaseCannon(resources['ship'])], [], 4);
   }
-  static createBaseEnemyShip(x,y){
-    return new BaseEnemyShip(x, y, resources['ship'], [], [], 2, new LinearPath(x, y, 270));
+  static createBaseEnemyShip(x,y, direction){
+    return new BaseEnemyShip(x, y, resources['ship'], [], [], 2, new LinearPath(x, y, direction));
   }
 }
 
--- RocketX/public/game_classes/Factories/WeaponFactory.js
+++ RocketX/public/game_classes/Factories/WeaponFactory.js
@@ -4,7 +4,7 @@
 'use strict'
 class WeaponFactory {
   static createBaseCannon(ship_img) {
-    return new BaseCannon(new BaseAmmo(1, resources['base_laser'], 2), 1, ship_img.width, ship_img.height);
+    return new BaseCannon(new BaseAmmo(1, resources['laser_2'], 2), 1, ship_img.width, ship_img.height);
   }
 }
 
