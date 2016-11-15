d018f1fe7fb24aa556403af1ed5b108955a89c42
Gerike <Gerike1995@gmail.com>
2016-11-15 11:42:47 +0100

    Implemented paths for enemy ships
    Enemy ships can be created with a path variable, which it will follow
    till the end...

--- RocketX/public/game_classes/Ships/BaseEnemyShip.js
+++ RocketX/public/game_classes/Ships/BaseEnemyShip.js
@@ -2,18 +2,25 @@
  * Created by Geri on 2016. 11. 15..
  */
 class BaseEnemyShip extends SpaceShip {
-  constructor(x, y, img, weapons, extras, speed) {
+  constructor(x, y, img, weapons, extras, speed, path=null) {
     super(x, y, img);
     this.weapons = weapons;
     this.extras = extras;
-    this.speed = speed;
+    this.path = path
   }
 
   draw(ctx) {
     ctx.drawImage(this.img, this.x, this.y);
   }
 
-  frame(){}
+  frame(){
+    if (this.path !== null)
+    {
+      let next_waypoint = this.path.getNextWayPoint().next().value;
+      this.x = next_waypoint.x;
+      this.y = next_waypoint.y;
+    }
+  }
 
   shoot(){}
 
