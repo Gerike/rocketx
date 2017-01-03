0985b1d83c55ea1128d310e107e43bb41666d55b
Kovács Gergő <Gerike1995@gmail.com>
2017-01-03 23:30:23 +0100

    Added "Latest changes" on the welcome screen

--- public/welcome.js
+++ public/welcome.js
@@ -24,6 +24,20 @@ function loadHighScores(count, time){
   );
 }
 
+function loadLatestNews(){
+  $.ajax({
+    url: 'https://api.github.com/repos/gerike/rocketx/commits',
+    method: 'GET'
+  }).done( (data) => {
+    for(let i = 0; i < 5; i++){
+      let date = new Date(data[i].commit.committer.date)
+      document.getElementById('ln_date' + i).innerHTML = date.getFullYear() + '.' + (date.getMonth() +1) + '.' + date.getDay();
+      document.getElementById('ln_message' + i).innerHTML = data[i].commit.message;
+    }
+  });
+}
+
 $(document).ready( () => {
-  loadHighScores(6, 'Daily')
+  loadLatestNews();
+  loadHighScores(6, 'Daily');
 });
