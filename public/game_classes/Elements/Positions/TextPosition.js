2e0f4b740fb6891696b23e888d45f51652b02399
Kovács Gergő <Gerike1995@gmail.com>
2017-01-06 22:52:35 +0100

    Extracted Position from TextPosition
    The base class now supports Entity management too

--- public/game_classes/Elements/Positions/TextPosition.js
+++ public/game_classes/Elements/Positions/TextPosition.js
@@ -1,53 +1,21 @@
-let POSITIONS = {};
-
-POSITIONS.X = {
-  'RIGHT': {value: 1, prepareContext: function(ctx) {ctx.textAlign="right"}},
-  'CENTER': {value: 2, prepareContext: function(ctx) {ctx.textAlign="center"}},
-  'LEFT': {value: 3, prepareContext: function(ctx) {ctx.textAlign="left"}},
-};
-
-POSITIONS.Y = {
-  'TOP': {value: 4, prepareContext: function(ctx) {ctx.textBaseline="top"}},
-  'CENTER': {value: 5, prepareContext: function(ctx) {ctx.textBaseline="middle"}},
-  'BOTTOM': {value: 6, prepareContext: function(ctx) {ctx.textBaseline="bottom"}},
-};
-
-class TextPosition {
+class TextPosition extends Position{
   constructor(xPos, yPos){
-      if (isNaN(xPos)) {
-        this.xAlign = xPos;
-        this.yAlign = yPos;
-        this.offset = 20;
-        switch(this.xAlign.value){
-          case 1: this.x = framework.constants.canvasWidth - this.offset; break;
-          case 2: this.x = framework.constants.canvasWidth/2; break;
-          case 3: this.x = this.offset; break;
-          default: throw 'Unsupported position'; break;
-        }
-        switch(this.yAlign.value){
-          case 4: this.y = this.offset; break;
-          case 5: this.y = framework.constants.canvasHeight/2; break;
-          case 6: this.y = framework.constants.canvasHeight - this.offset; break;
-          default: throw 'Unsupported position'; break;
-        }
-      }
-      else {
-        this.x = xPos;
-        this.y = yPos;
-        this.xAlign = POSITIONS.X.LEFT;
-        this.yAlign = POSITIONS.Y.TOP;
-      }
+      super(xPos, yPos);
   }
 
-  getX(){
-    return this.x;
-  }
-  getY(){
-    return this.y;
-  }
   prepareContext(ctx){
-    this.xAlign.prepareContext(ctx);
-    this.yAlign.prepareContext(ctx);
+    switch(this.xAlign){
+      case 'RIGHT': ctx.textAlign="right"; break;
+      case 'CENTER': ctx.textAlign="center"; break;
+      case 'LEFT': ctx.textAlign="left"; break;
+      default: throw 'Unsupported position'; break;
+    }
+    switch(this.yAlign){
+      case 'TOP': ctx.textBaseline="top"; break;
+      case 'CENTER': ctx.textBaseline="middle"; break;
+      case 'BOTTOM': ctx.textBaseline="bottom"; break;
+      default: throw 'Unsupported position'; break;
+    }
   }
 
 }
