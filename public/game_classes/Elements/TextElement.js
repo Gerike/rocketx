99b0975e5a93c113860b06e35ef85ea8d925ece1
Kovács Gergő <Gerike1995@gmail.com>
2017-01-07 14:00:53 +0100

    Added Transitions and Style for TextElements
    TextElements can recieve Transitions and Style which can change the
    context attributes (like fillStyle, font, etc..)

--- public/game_classes/Elements/TextElement.js
+++ public/game_classes/Elements/TextElement.js
@@ -1,14 +1,19 @@
 class TextElement extends Element {
-  constructor(position, expirationChecker, text, font = "60px Georgia", color = "white") {
+  constructor(position, expirationChecker, text, style, inTransition = null, outTransition = null) {
     super(position, expirationChecker);
     this.text = text;
-    this.font = font;
-    this.color = color;
+    this.style = style;
+    this.inTransition = inTransition;
+    this.outTransition = outTransition;
+    if (this.outTransition) outTransition.activateOnStartingFrame(this.expirationChecker.getExpirationFrame());
   }
 
   _draw(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.font = this.font;
+    if (this.inTransition !== null && !this.inTransition.isDone())
+      this.inTransition.prepareContext(ctx);
+    if (this.outTransition !== null && this.outTransition.activated)
+      this.outTransition.prepareContext(ctx);
+    this.style.prepareContext(ctx);
     this.position.prepareContext(ctx);
     ctx.fillText(this.text, this.position.getX(), this.position.getY());
   }
