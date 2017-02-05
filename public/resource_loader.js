f6e3d8467a12c42c2a25e2420d94c3e2a388ff8e
Kovács Gergő <Gerike1995@gmail.com>
2017-02-05 17:32:13 +0100

    Added Tutorial Boss image instead of placeholder

--- public/resource_loader.js
+++ public/resource_loader.js
@@ -5,7 +5,7 @@
 
 const canvas = document.getElementById('game_canvas');
 const ctx = canvas.getContext('2d');
-const resources_location = ['/assets/laser_4.png', '/assets/ship.png', '/assets/base_laser.png', '/assets/laser_2.png', '/assets/ship_2.png', 'assets/ship_3.png', '/assets/laser_3.png', '/assets/keyboard_arrows.png', '/assets/keyboard_space.png', '/assets/shield.png'];
+const resources_location = ['/assets/laser_4.png', '/assets/ship.png', '/assets/base_laser.png', '/assets/laser_2.png', '/assets/ship_2.png', 'assets/ship_3.png', '/assets/laser_3.png', '/assets/keyboard_arrows.png', '/assets/keyboard_space.png', '/assets/shield.png', '/assets/tutorial_boss.png'];
 
 var entities = [];
 var resources = {};
