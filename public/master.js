8adab74adf7f4c9b0190552cda6cfb98c2d6d25b
Kovács Gergő <Gerike1995@gmail.com>
2017-03-25 13:41:37 +0100

    Added navigation sidebar

--- public/master.js
+++ public/master.js
@@ -1,6 +1,6 @@
 'use strict';
 $(document).ready(() => {
-  $('#__login_button').click( e => {
+  $('#__login_button').click(e => {
     e.preventDefault();
     let request = $.ajax({
       method: "POST",
@@ -8,13 +8,30 @@ $(document).ready(() => {
       data: $('#login_form').serialize(),
     });
 
-    request.fail( data => console.log(data));
+    request.fail(data => console.log(data));
 
-    request.done( () => {
+    request.done(() => {
       location.reload();
     });
   });
 
-  $('#play_button').on('click', () => window.location.href = "/play");
+  $('#_sidebar_play').on('click', () => window.location.href = '/play');
+  $('#_sidebar_news').on('click', () => window.location.href = '/');
+  $('#_open_sidebar_button').on('click', () => _toggleSidebar());
 });
 
+
+function _toggleSidebar() {
+  let sideBar = $('#_sidebar');
+  let toggleButton = $('#_open_sidebar_button');
+  let slideDirection = $(window).width() <= 703 ? 'up' : 'right';
+
+  if (sideBar.is(':visible')) {
+    sideBar.hide('slide', {direction: slideDirection}, 600);
+    toggleButton.html('<i class="fa fa-bars" aria-hidden="true"></i>');
+  }
+  else {
+    sideBar.show('slide', {direction: slideDirection}, 600);
+    toggleButton.html('<i class="fa fa-times" aria-hidden="true"></i>');
+  }
+}
