01e8dd10ff6d5775e634d31e8eb3b5125b70e2b0
Kovács Gergő <Gerike1995@gmail.com>
2017-03-25 13:42:23 +0100

    Better design on low resolution devices

--- public/welcome.js
+++ public/welcome.js
@@ -4,40 +4,40 @@
 'use strict';
 const TOP_SCORE_ROW = 6;
 
-function loadHighScores(count, time){
- /* $.ajax({
-      url: '/rest/scores/' + count,
-      method: 'GET'
-  }).done( (data) => {
-        data = JSON.parse(data);
-        for(let i = 0; i < data.length; i++){
-          document.getElementById('toplist' + i).innerHTML = data[i].username;
-          document.getElementById('toplist_score' + i).innerHTML = data[i].points;
-        }
-        if (data.length < TOP_SCORE_ROW - 1){
-          for(let i = data.length; i < TOP_SCORE_ROW; i++){
-            document.getElementById('toplist' + i).innerHTML = "";
-            document.getElementById('toplist_score' + i).innerHTML = "";
-          }
-        }
-    }
-  );*/
+function loadHighScores(count, time) {
+  /* $.ajax({
+   url: '/rest/scores/' + count,
+   method: 'GET'
+   }).done( (data) => {
+   data = JSON.parse(data);
+   for(let i = 0; i < data.length; i++){
+   document.getElementById('toplist' + i).innerHTML = data[i].username;
+   document.getElementById('toplist_score' + i).innerHTML = data[i].points;
+   }
+   if (data.length < TOP_SCORE_ROW - 1){
+   for(let i = data.length; i < TOP_SCORE_ROW; i++){
+   document.getElementById('toplist' + i).innerHTML = "";
+   document.getElementById('toplist_score' + i).innerHTML = "";
+   }
+   }
+   }
+   );*/
 }
 
-function loadLatestNews(){
+function loadLatestNews() {
   $.ajax({
     url: 'https://api.github.com/repos/gerike/rocketx/commits',
     method: 'GET'
-  }).done( (data) => {
-    for(let i = 0; i < 5; i++){
+  }).done((data) => {
+    for (let i = 0; i < 5; i++) {
       let date = new Date(data[i].commit.committer.date)
-      document.getElementById('ln_date' + i).innerHTML = date.getFullYear() + '.' + (date.getMonth() +1) + '.' + date.getDay();
+      document.getElementById('ln_date' + i).innerHTML = date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDay();
       document.getElementById('ln_message' + i).innerHTML = data[i].commit.message;
     }
   });
 }
 
-$(document).ready( () => {
+$(document).ready(() => {
   loadLatestNews();
   loadHighScores(6, 'Daily');
 });
