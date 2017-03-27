2052cce55cd3996499361bebcc306eacf7a1a5a9
Kovács Gergő <Gerike1995@gmail.com>
2017-03-27 23:49:42 +0200

    Added warning for forum not working yet

--- public/welcome.js
+++ public/welcome.js
@@ -9,9 +9,8 @@ function loadLatestNews() {
     url: 'https://api.github.com/repos/gerike/rocketx/commits?sha=visual_update',
     method: 'GET'
   }).done((data) => {
-    console.log(data)
     for (let i = 0; i < 5; i++) {
-      let date = new Date(data[i].commit.committer.date)
+      let date = new Date(data[i].commit.committer.date);
       document.getElementById('ln_date' + i).innerHTML = date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDay();
       document.getElementById('ln_message' + i).innerHTML = data[i].commit.message;
     }
