c60f3c3da30bd36470dd050c596820f7cdfe14d6
Kovács Gergő <Gerike1995@gmail.com>
2017-03-27 23:30:04 +0200

    Sidebar / Messaging scripts will only load if the user is logged in

--- public/master.js
+++ public/master.js
@@ -12,35 +12,8 @@ $(document).ready(() => {
       location.reload();
     });
   });
-
-  $('#_sidebar_play').on('click', () => window.location.href = '/play');
-  $('#_sidebar_news').on('click', () => window.location.href = '/');
-  $('#_sidebar_logout').on('click', () => window.location.href = '/logout');
-  $('#_open_sidebar_button').on('click', () => _toggleSidebar());
-
-  _updateUnreadMessageCounter();
-  setInterval(() => {
-    _updateUnreadMessageCounter();
-  }, 10000);
 });
 
-
-function _toggleSidebar() {
-  let sideBar = $('#_sidebar');
-  let toggleButton = $('#_open_sidebar_button');
-  let slideDirection = $(window).width() <= 703 ? 'up' : 'right';
-
-  if (sideBar.is(':visible')) {
-    sideBar.hide('slide', {direction: slideDirection}, 600);
-    toggleButton.html('<i class="fa fa-bars" aria-hidden="true"></i>');
-    toggleButton.removeClass('_transparent_after');
-  }
-  else {
-    sideBar.show('slide', {direction: slideDirection}, 600);
-    toggleButton.html('<i class="fa fa-times" aria-hidden="true"></i>');
-    toggleButton.addClass('_transparent_after');
-  }
-}
 
 function _createNotification(type, text) {
   let icon;
@@ -69,26 +42,3 @@ function _createNotification(type, text) {
   }, 4000);
 }
 
-function _writeUnreadMesssageCounter(count) {
-  const messageCounter = $('#_sidebar_unread_message_count');
-  const sidebarButton = $('#_open_sidebar_button');
-
-  if (parseInt(count) > 0) {
-    messageCounter.html('(' + count + ')');
-    messageCounter.parent().addClass('_sidebar_link_important');
-  }
-  else {
-    messageCounter.html('');
-    messageCounter.parent().removeClass('_sidebar_link_important');
-  }
-
-  sidebarButton.attr('data-notifications', count);
-}
-
-function _updateUnreadMessageCounter() {
-  $.ajax({
-    url: '/messages/unread_count',
-    method: 'GET',
-    success: data => _writeUnreadMesssageCounter(data.count)
-  });
-}
