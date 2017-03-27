4fe894c8b7844615c850aaa292dff4ab402a928b
Kovács Gergő <Gerike1995@gmail.com>
2017-03-27 22:40:26 +0200

    Updated User class for easier using messages

--- app/Model/User.js
+++ app/Model/User.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Lucid = use('Lucid');
+const Message = use('App/Model/Message');
 
 class User extends Lucid {
 
@@ -10,6 +11,18 @@ class User extends Lucid {
 
   messages (){
     return this.hasMany('App/Model/Message');
+  }
+
+  * getSentMessages(){
+      return (yield Message.query().where('sender_id', this.id).fetch());
+  }
+
+  * getRecievedMessages(){
+    return (yield Message.query().where('recipient_id', this.id).fetch());
+  }
+
+  * getUnreadMessages(){
+    return (yield Message.query().where('recipient_id', this.id).where('unread', 'true').fetch());
   }
 
 }
